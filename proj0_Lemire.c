96be02bd30198a0848844eefdf7781ca367b86d7
Nicholas Lemire <nlemire@gmail.com>
2020-11-28 21:59:11 -0500

    Add files via upload

---
+++ proj0_Lemire.c
@@ -0,0 +1,38 @@
+/* 
+* Nicholas Lemire
+* CIS 205 – P1 
+* Project 0
+*
+* Prints initials in block letters down the page 
+*/
+
+#include <stdio.h>
+
+int main( void ){
+
+  puts( "NNNNNNNNNNNNNNNNNNNN" );
+  puts( "                 N" );
+  puts( "               N" );
+  puts( "             N" );
+  puts( "           N" );
+  puts( "         N" );
+  puts( "       N" );
+  puts( "     N" );
+  puts( "   N" );
+  puts( " N" );
+  puts( "NNNNNNNNNNNNNNNNNN" );
+    puts( "\n" );
+  puts( "LLLLLLLLLLLLLLLLLLLL" );
+  puts( "L" );
+  puts( "L" );
+  puts( "L" );
+  puts( "L" );
+  puts( "L" );
+  puts( "L" );
+  puts( "L" );
+  puts( "L" );
+  puts( "L" );
+  puts( "L" );
+
+return ( 0 );
+}
